b2f3ffed5a17166dc93468b6c2ae7372b48c92ce
Ian Maynard <38901355+Relreo@users.noreply.github.com>
2022-04-01 17:23:40 -0700

    Updated PCCraftingRecord.lua
    Updated Output String Formatting to not Include "s" at the end of item name

--- PCCraftingRecord.lua
+++ PCCraftingRecord.lua
@@ -19,7 +19,7 @@ end
 function PCCraftingRecord:getPrintableFormat()
     local bonusItems = self.amountCrafted - self.numCrafts
     local procRate = string.format("%.2f",(bonusItems / self.numCrafts) * 100)
-    return self.crafter.." made "..self.amountCrafted.." "..self.item.."s in "..self.numCrafts.." crafts.\nProc Rate: "..procRate.."%\nNumber of bonus items: "..bonusItems
+    return self.crafter.." made "..self.amountCrafted.." "..self.item.." in "..self.numCrafts.." crafts.\nProc Rate: "..procRate.."%\nNumber of bonus items: "..bonusItems
 end
 ---------------------------
 
